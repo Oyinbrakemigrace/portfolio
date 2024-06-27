2678df675d308c445450f761536550bbafd1fde5
Grace <graceofubu04@gmail.com>
2024-06-28 00:09:11 +0100

    fixed prod err

--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -14,16 +14,14 @@ function Intro() {
                 <h3 className="text-2xl py-2 text-zinc-700 dark:text-white">Front-End Web Developer</h3>
                 <h4 className='text-zinc-700 dark:text-zinc-300 text-start mt-10 lg:mt-20 font-bold text-lg lg:text-3xl'>Favourite Quote:</h4>
                 <p className="font-medium text-start lg:text-xl py-2 leading-8 dark:text-zinc-200 italic text-gray-800">
-                    "You never lose, You either win or You learn."
+                    &quot;You never lose, You either win or You learn.&quot;
                 </p>
             </div>
             <div className="relative lg:w-1/2 h-full mt-4 mx-auto overflow-hidden">
-                <div className="relative ">
+                <div className="relative">
                     <Image src={grace} alt="myImage" className="lg:w-screen lg:h-screen w-full h-full m-auto rounded-full" />
                 </div>
             </div>
-
-
         </div>
     )
 }
