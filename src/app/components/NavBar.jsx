c98447488ee41a031567a78f94acc3a7e92fd071
Grace <graceofubu04@gmail.com>
2024-06-27 14:12:35 +0100

    navbar and intro

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,20 +1,35 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoMoon } from 'react-icons/io5'
 
 function NavBar({setDarkMode, darkMode}) {
+    const [show, setShow] = useState(false)
+
+    const handleTransition = () => {
+        if (window.scrollY > 100) {
+            setShow(true)
+        } else {
+            setShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleTransition)
+        return () => window.removeEventListener('scroll', handleTransition)
+    }, [])
     return (
        <div className="lg:pb-10 pb-20">
-            <nav className="py-4 flex justify-between bg-gray-400/20 lg:px-10 px-4 fixed left-0 w-full">
+            <nav className={`py-4 flex justify-between bg-[#AD7FAD] bg-opacity-30 lg:px-10 px-4 fixed left-0 w-full ${!show ? "transition-all duration-500 ease-in bg-opacity-0" : ""
+          }`}>
                 <h1 className="text-xl">grace</h1>
                 <div className="flex items-center">
-                    <div>
+                    <div className='active:scale-105 transition-all'>
                         <IoMoon onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
                     </div>
-                    <div>
+                    <div className='active:scale-105 transition-all ease-out duration-500'>
                         <a
                             href="#"
-                            className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2  rounded-md ml-8"
+                            className="bg-gradient-to-r from-[#AD7FAD] to-[#F1B2AA] text-white px-4 py-2  rounded-md ml-8"
                         >
                             Resume
                         </a>
--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 
 export default function Home() {
-  const[darkMode, setDarkMode] = useState(false)
+  const[darkMode, setDarkMode] = useState(true)
   return (
     <div className={darkMode ? 'dark': ''}>
       <main className=" bg-white px-10 dark:bg-gray-900">
-        <section className="min-h-screen">
+        <section>
           <NavBar setDarkMode={setDarkMode} darkMode={darkMode}/>
           <Intro/>
         </section>
         <section>
-          
+
         </section>
       </main>
     </div>
